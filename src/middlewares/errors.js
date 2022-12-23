ffc878b3bc830e4384622558626a1bd8939cc2ed
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2022-12-23 14:18:43 -0300

    create error handler

---
+++ src/middlewares/errors.js
@@ -0,0 +1,12 @@
+const errors = {
+    ValidationError: 400,
+    CastError: 400
+  }
+  
+  module.exports = {
+      handler (err, req, res, next) {
+          console.log(err.message)
+          res.status(errors[err.name]).send(err.message)
+        }
+  }
+  
