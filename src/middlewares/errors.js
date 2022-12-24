f03a39a6314c9c62112aeb2ba5dc7b75eedec63c
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2022-12-24 01:16:04 -0300

    add new errors handlers

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -1,11 +1,11 @@
 const errors = {
-    ValidationError: 400,
-    CastError: 400
-  }
+  ValidationError: 400,
+  CastError: 400,
+  AxiosError: 400
+}
 
   module.exports = {
       handler (err, req, res, next) {
-          console.log(err.message)
           res.status(errors[err.name]).send(err.message)
         }
   }
