92478131ba418bf0eec139b7c20c638349786325
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2022-12-27 21:54:11 -0300

    Create redisConfig.js

---
+++ src/redis/redisConfig.js
@@ -0,0 +1,20 @@
+const Redis = require('redisio')
+const { promisify } = require('util')
+
+const redisClient = new Redis()
+
+function getRedis (value) {
+    const syncRedisGet = promisify(redisClient.get).bind(redisClient)
+    return syncRedisGet(value)
+}
+
+function setRedis (key, value) {
+    const syncRedisSet = promisify(redisClient.set).bind(redisClient)
+    return syncRedisSet(key, value)
+}
+
+module.exports = {
+    redisClient,
+    getRedis,
+    setRedis
+}
